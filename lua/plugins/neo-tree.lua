9ebedd9b002960ec6f4afb6bba838eee0e312a68
Ali Yuksekkaya <alisyuksekkaya@gmail.com>
2024-05-25 16:15:49 -0700

    add formatter

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,29 +1,27 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-	
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+
 	config = function()
-         require("neo-tree").setup({
-        event_handlers = {
+		require("neo-tree").setup({
+			event_handlers = {
 
-          {
-            event = "file_opened",
-            handler = function(file_path)
-              -- auto close
-              -- vimc.cmd("Neotree close")
-              -- OR
-              require("neo-tree.command").execute({ action = "close" })
-            end
-          },
-
-        }
-      })
-	 vim.keymap.set('n', '<leader>	', ':Neotree<CR>', {})
-	end
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>	", ":Neotree<CR>", {})
+	end,
 }
-
