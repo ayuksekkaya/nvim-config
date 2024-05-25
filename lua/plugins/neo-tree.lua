c9bf126663940bca88a43e0de205a36048acf22b
Ali Yuksekkaya <alisyuksekkaya@gmail.com>
2024-05-25 15:36:43 -0700

    added more configs

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,21 @@ return {
     },
 	
 	config = function()
+         require("neo-tree").setup({
+        event_handlers = {
+
+          {
+            event = "file_opened",
+            handler = function(file_path)
+              -- auto close
+              -- vimc.cmd("Neotree close")
+              -- OR
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+
+        }
+      })
 	 vim.keymap.set('n', '<leader>	', ':Neotree<CR>', {})
 	end
 }
