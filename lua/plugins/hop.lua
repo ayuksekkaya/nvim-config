f241b243e5eb0e0f6f867ccebc7869508d952828
Ali Yuksekkaya <alisyuksekkaya@gmail.com>
2024-05-26 12:21:18 -0700

    added more lsp features

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,10 @@
+return {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {
+        keys = 'etovxqpdygfblzhckisuran'
+    },
+	keys = {
+			{ "tr",  ":HopWord<cr>",  "Hop word" },
+		},
+}
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,31 +2,37 @@ return {
 	{
 		"williamboman/mason.nvim",
 		config = function()
-			require("mason").setup()
+			require("mason").setup({ PATH = "prepend" })
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
 		config = function()
-			require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "tsserver" } })
+			require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "tsserver", "omnisharp" } })
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
 		config = function()
-			--- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
-
-		---	lspconfig.omnisharp.setup({
-	---			capabilities = capabilities,
-------				enable_roslyn_analysers = true,
----				enable_import_completion = true,
----				organize_imports_on_format = true,
----				enable_decompilation_support = true,
----				filetypes = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
----			})
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.omnisharp.setup({
+				capabilities = capabilities,
+				enable_roslyn_analysers = true,
+				enable_import_completion = true,
+				organize_imports_on_format = true,
+				enable_decompilation_support = true,
+				filetypes = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
